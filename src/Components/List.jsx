19a3157d1e27904423c4a7c926911a7e15cb4287
George Sierra <galixig3n@gmail.com>
2017-04-10 16:56:28 +0300

    .

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -4,20 +4,6 @@ import StoryCard from '../Containers/StoryCard';
 
 export default class List extends React.PureComponent {
   render() {
-    return (
-      <div className="list">
-        {this.props.list.map(story => (
-          <StoryCard
-            key={story.get('id')}
-            id={story.get('id')}
-            kids={story.get('kids')}
-            title={story.get('title')}
-            by={story.get('by')}
-            url={story.get('url')}
-            time={story.get('time')}
-          />
-        ))};
-      </div>
-    );
+    return false
   }
 }
